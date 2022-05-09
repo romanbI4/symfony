bead9cac5b60a1a2efcbce2b33c38a4a2154f064
romanbI4 <roman.vdovenko@idapgroup.com>
2022-05-09 17:54:12 +0300

    Add api doc.

--- backend/src/Controller/BookCategoryController.php
+++ backend/src/Controller/BookCategoryController.php
@@ -3,16 +3,26 @@
 namespace App\Controller;
 
 use App\Service\BookCategoryService;
+use App\Model\BookCategoryListResponse;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use OpenApi\Annotations as OA;
+use Nelmio\ApiDocBundle\Annotation\Model;
 
 class BookCategoryController extends AbstractController
 {
     public function __construct(private BookCategoryService $bookCategoryService)
     {}
 
-    #[Route('/api/v1/book/categories')]
+    /**
+     * @OA\Response(
+     *     response=200,
+     *     description="Return book categories",
+     * @Model(type=BookCategoryListResponse::class)
+     * )
+     */
+    #[Route('/api/v1/book/categories', ['GET'])]
     public function categories(): Response
     {
         return $this->json($this->bookCategoryService->getCategories());
